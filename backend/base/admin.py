60555ea3e050ba8fca6d73185c02d5d98929e0bd
Gautam0507 <110854761+Gautam0507@users.noreply.github.com>
2023-07-01 13:24:37 +0530

    extended user info adding addresss and phno

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,6 +1,22 @@
+from .models import Meter
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Userinfo
 # Register your models here.
 
-from .models import Meter
+
+class UserinfoInline(admin.StackedInline):
+    model = Userinfo
+    can_delete = False
+    verbose_name_plural = 'userinfo'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserinfoInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Meter)
--- backend/base/models.py
+++ backend/base/models.py
@@ -4,10 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Userinfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    Address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+
+
 class Meter(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    Serial_number = models.IntegerField(unique= True)
+    Serial_number = models.IntegerField(unique=True)
     Last_billed_reading = models.IntegerField()
     Last_recorded_reading = models.IntegerField()
     Last_updated_time = models.DateTimeField(auto_now=True)
-
